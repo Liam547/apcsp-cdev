5e9d10d612d9bafecde3cd1e176428a4421747dc
Liam Bowman <bowmanl1560@my.sduhsd.net>
2021-01-07 04:59:40 +0000

    variables.c commit

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+
+  printf("%d (was a) is now C and %d (was c) is now A\n", c, a);
+}
